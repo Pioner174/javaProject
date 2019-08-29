87b71097199a1a447424602a7d82b596d6bc1a0b
Romashka951 <Romashka951@gmail.com>
2019-08-29 15:07:02 +0500

    Vector

--- Vector/src/Main.java
+++ Vector/src/Main.java
@@ -5,12 +5,28 @@ public  class Main {
     Main main = new Main();
      Map<String, double[]> vectors = new HashMap<String, double[]>();
 //    System.out.println(Arrays.toString(main.createK()));
+        Scanner scan = new Scanner(System.in);
 
         while (true){
             System.out.println("Cозданные векторы: ");
             main.display(vectors);
-            vectors.put( main.puty(), main.createK());
-
+            System.out.println("Выберите пунк из меню: \n" + "1) Добавить вектор;   " + "2) Вычислить длину вектора;\n" + "3) Вычисляющий скалярное произведение двух векторов;");
+            switch (scan.nextInt()) {
+                case (1):
+                    vectors.put(main.puty(),main.createK());
+                    break;
+                case (2):
+                    System.out.println("Введите название вектора:");
+                    System.out.println("Результат: " + main.LongVector(vectors.get(scan.next())));
+                    break;
+                case (3):
+                    System.out.println("Введите название 1-го вектора:");
+                    String a = scan.next();
+                    System.out.println("Введите название 2-го вектора:");
+                    String b = scan.next();
+                    System.out.println("Результат: " + main.scproiz(vectors.get(a),vectors.get(b)));
+                    break;
+            }
         }
     }
     public double[] createK(){
@@ -40,6 +56,8 @@ public  class Main {
     public double LongVector(double[]  k){
     return Math.sqrt(k[0]*k[0]+k[1]*k[1]+k[2]*k[2]);
     }
-
+    public double scproiz(double[]  k,double[]  v){
+       return k[0]*v[0]+k[1]*v[1]+k[2]*v[2];
+    }
 
 }
