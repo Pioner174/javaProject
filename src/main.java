a93c862e8bdcb00d03b624b8df6d202c28ed6883
Romas <Romashka951@gmail.com>
2019-09-12 22:34:18 +0500

    TestHH

--- src/main.java
+++ src/main.java
@@ -1,10 +1,15 @@
 import java.util.*;
 public class main {
+    public static void main(String[] args){
+    main main = new main();
+    Scanner scan = new Scanner(System.in);
+    System.out.println(Arrays.toString(main.in(scan.nextInt())));
 
 
+}
     public int[] in (int chislo){
-        
-        int[] out =
-        return 0;
+
+        int[] out ={chislo-3,chislo-1,chislo,chislo+1,chislo+3};
+        return out;
     }
 }
